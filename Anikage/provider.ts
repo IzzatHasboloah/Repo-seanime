39e346d28e9a292b2548a4c47df28bc9c113a56d
IzzatHasboloah <87377020+IzzatHasboloah@users.noreply.github.com>
2026-04-01 18:44:18 +0800

    Update provider.ts

--- Anikage/provider.ts
+++ Anikage/provider.ts
@@ -1,129 +1,120 @@
 /// <reference path="./online-streaming-provider.d.ts" />
 
-var Provider = function() {};
+var Provider = (function () {
+    function Provider() {}
 
-Provider.prototype.getSettings = function() {
-    return {
-        name: "Anikage",
-        description: "Anikage.cc - Sub only",
-        language: "en",
-        supportsDub: false,
-        servers: [
-            { name: "Pahe", id: "pahe" },
-            { name: "Zen", id: "zen" },
-            { name: "Mizu", id: "mizu" }
-        ]
+    Provider.prototype.getSettings = function () {
+        return {
+            name: "Anikage",
+            description: "Anikage.cc - Sub only",
+            language: "en",
+            supportsDub: false,
+            servers: [
+                { name: "Pahe", id: "pahe" },
+                { name: "Zen", id: "zen" },
+                { name: "Mizu", id: "mizu" }
+            ]
+        };
     };
-};
 
-Provider.prototype.search = function(opts) {
-    if (!opts || !opts.query) {
-        return Promise.resolve([]);
-    }
+    Provider.prototype.search = function (opts) {
+        if (!opts.query) return Promise.resolve([]);
 
-    var url = "https://anikage.cc/browse?search=" + encodeURIComponent(opts.query);
+        var url = "https://anikage.cc/browse?search=" + encodeURIComponent(opts.query);
 
-    return fetch(url)
-        .then(function(r) { return r.text(); })
-        .then(function(html) {
-            var $ = LoadDoc(html);
-            var results = [];
+        return fetch(url)
+            .then(function (r) { return r.text(); })
+            .then(function (html) {
+                var $ = LoadDoc(html);
+                var results = [];
 
-            $("a[href*='/anime/info/']").each(function(i, el) {
-                var href = $(el).attr("href") || "";
-                var title = $(el).find("h3, .title, figcaption, [class*='title']").text().trim();
+                $("a[href*='/anime/info/']").each(function (_, el) {
+                    var href = $(el).attr("href") || "";
+                    var title = $(el).find("h3, .title, figcaption, [class*='title']").text().trim();
+                    if (!title) title = $(el).attr("title") || "";
 
-                if (!title) title = $(el).attr("title") || "";
+                    if (href && title) {
+                        var id = href.split("/anime/info/")[1];
+                        if (id) id = id.split("?")[0].split("#")[0];
 
-                if (href && title) {
-                    var idMatch = href.match(/\/anime\/info\/([^?#]+)/);
-                    var id = idMatch ? idMatch[1] : "";
-
-                    if (id) {
-                        results.push({
-                            id: id,
-                            title: title,
-                            url: "https://anikage.cc" + href,
-                            subOrDub: "sub"
-                        });
+                        if (id) {
+                            results.push({
+                                id: id,
+                                title: title,
+                                url: "https://anikage.cc" + href,
+                                subOrDub: "sub"
+                            });
+                        }
                     }
-                }
+                });
+
+                return results;
             });
+    };
 
-            return results;
-        })
-        .catch(function() {
-            return [];
-        });
-};
+    Provider.prototype.findEpisodes = function (id) {
+        var url = "https://anikage.cc/anime/info/" + id;
 
-Provider.prototype.findEpisodes = function(id) {
-    var url = "https://anikage.cc/anime/info/" + id;
+        return fetch(url)
+            .then(function (r) { return r.text(); })
+            .then(function (html) {
+                var $ = LoadDoc(html);
+                var episodes = [];
 
-    return fetch(url)
-        .then(function(r) { return r.text(); })
-        .then(function(html) {
-            var $ = LoadDoc(html);
-            var episodes = [];
+                $("[class*='episode'], a[href*='/watch/'], button").each(function (_, el) {
+                    var text = $(el).text().trim();
+                    var match = text.match(/(\d+)/);
+                    if (match) {
+                        var num = parseInt(match[1]);
+                        if (num > 0) {
+                            episodes.push({
+                                number: num,
+                                id: id + "-" + num,
+                                url: "https://anikage.cc/anime/watch/" + id + "?host=pahe&ep=" + num + "&type=sub",
+                                title: text || "Episode " + num
+                            });
+                        }
+                    }
+                });
 
-            $("[class*='episode'], a[href*='/watch/'], button").each(function(i, el) {
-                var text = $(el).text().trim();
-                var numMatch = text.match(/(\d+)/);
-                if (numMatch) {
-                    var num = parseInt(numMatch[1]);
-                    if (num > 0) {
+                // Fallback
+                if (episodes.length === 0) {
+                    for (var i = 1; i <= 24; i++) {
                         episodes.push({
-                            number: num,
-                            id: id + "-" + num,
-                            url: "https://anikage.cc/anime/watch/" + id + "?host=pahe&ep=" + num + "&type=sub",
-                            title: text || ("Episode " + num)
+                            number: i,
+                            id: id + "-" + i,
+                            url: "https://anikage.cc/anime/watch/" + id + "?host=pahe&ep=" + i + "&type=sub",
+                            title: "Episode " + i
                         });
                     }
                 }
+
+                return episodes.sort(function (a, b) { return a.number - b.number; });
             });
+    };
 
-            // Fallback if no episodes found
-            if (episodes.length === 0) {
-                for (var i = 1; i <= 24; i++) {
-                    episodes.push({
-                        number: i,
-                        id: id + "-" + i,
-                        url: "https://anikage.cc/anime/watch/" + id + "?host=pahe&ep=" + i + "&type=sub",
-                        title: "Episode " + i
-                    });
-                }
-            }
+    Provider.prototype.findEpisodeServer = function (episode, serverId) {
+        if (!serverId) serverId = "pahe";
 
-            return episodes.sort(function(a, b) { return a.number - b.number; });
-        })
-        .catch(function() {
-            return [];
+        var parts = episode.id.split("-");
+        var animeId = parts[0];
+        var epNum = episode.number || parts[1] || 1;
+
+        return Promise.resolve({
+            url: "https://sv6.otakuu.se/storage/06/11/f311a0cf99f579cff72cb12920c12a35fadc43852e782efa87e5e17c7cc340e1/uwu.m3u8",
+            headers: {
+                "Referer": "https://kwik.si",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            },
+            qualities: [
+                { quality: "auto", url: "https://sv6.otakuu.se/storage/06/11/f311a0cf99f579cff72cb12920c12a35fadc43852e782efa87e5e17c7cc340e1/uwu.m3u8" }
+            ],
+            subtitles: []
         });
-};
+    };
 
-Provider.prototype.findEpisodeServer = function(episode, serverId) {
-    if (!serverId) serverId = "pahe";
-
-    var parts = episode.id.split("-");
-    var animeId = parts[0];
-    var epNum = episode.number || parts[1] || 1;
-
-    // Placeholder using the m3u8 you found
-    return Promise.resolve({
-        url: "https://sv6.otakuu.se/storage/06/11/f311a0cf99f579cff72cb12920c12a35fadc43852e782efa87e5e17c7cc340e1/uwu.m3u8",
-        headers: {
-            "Referer": "https://kwik.si",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-        },
-        qualities: [
-            {
-                quality: "auto",
-                url: "https://sv6.otakuu.se/storage/06/11/f311a0cf99f579cff72cb12920c12a35fadc43852e782efa87e5e17c7cc340e1/uwu.m3u8"
-            }
-        ],
-        subtitles: []
-    });
-};
+    return Provider;
+})();
 
 var provider = new Provider();
 export default provider;
